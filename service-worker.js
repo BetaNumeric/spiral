44f3ca72ee37f63eaf7a7a99c6148d234cfc37ca
Tim <tim.paulawitz@gmail.com>
2025-11-15 23:56:45 +0100

    v1.3

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,49 @@
 // service-worker.js
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches
-      .open('v1')
-      .then(cache =>
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/icons/icon_192.png',
-          '/icons/icon_512.png',
-        ])
-      )
-  );
-});  // ← closes addEventListener
+const CACHE_NAME = 'spiral-v1';
+const toURL = (p) => new URL(p, self.location).toString();
+const ASSETS = [
+  './',
+  './index.html',
+  './main.js',
+  './manifest.json',
+  './icons/icon_32.png',
+  './icons/icon_192.png',
+  './icons/icon_512.png',
+  './sounds/click.mp3',
+  './sounds/click.wav',
+  './sounds/click0.mp3',
+];
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS.map(toURL)))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => 
+      cached || fetch(req).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          // Only cache GET and same-origin
+          if (req.method === 'GET' && new URL(req.url).origin === self.location.origin) {
+            cache.put(req, copy).catch(() => {});
+          }
+        });
+        return resp;
+      }).catch(() => cached)
+    )
   );
 });
