10c5f4cae36f016a75834e0d1a8342ac78ced81f
Tim <tim.paulawitz@gmail.com>
2026-03-15 14:05:12 +0100

    Update app-version.js

--- js/app-version.js
+++ js/app-version.js
@@ -2,7 +2,7 @@
 (function (globalScope) {
   'use strict';
 
-  const APP_VERSION = '1.36';
+  const APP_VERSION = '1.361';
   const APP_CACHE_PREFIX = 'spiral-v';
   const APP_CACHE_NAME = `${APP_CACHE_PREFIX}${APP_VERSION}`;
 
